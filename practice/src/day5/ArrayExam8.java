0e1a2ca91f4549a6595a528956bcebf95bba9083
st8324 <stajun@naver.com>
2019-04-01 16:58:17 +0900

    5일차-배열 선언에 방법에 따른 차이 에제

---
+++ practice/src/day5/ArrayExam8.java
@@ -0,0 +1,16 @@
+package day5;
+
+public class ArrayExam8 {
+
+	public static void main(String[] args) {
+		int [] arr1,arr2;
+		int arr3[],arr4;
+		arr1 = new int[3];//배열
+		arr2 = new int[3];//배열
+		arr3 = new int[3];//배열
+		//arr4 = new int[3];//변수 arr4 : int
+		int []arr5;
+		int arr6[];
+	}
+
+}
