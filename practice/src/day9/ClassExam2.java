b9c564c07e70d12b9433c0efca166a3eaf832bc3
st8324 <stajun@naver.com>
2019-04-05 12:08:13 +0900

    9일차-다른 패키지에 있는 클래스와 메소드 활용 예제

---
+++ practice/src/day9/ClassExam2.java
@@ -0,0 +1,18 @@
+package day9;
+
+import day8.MethodExam3;
+import day8.MethodExam4;
+
+public class ClassExam2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String []s = new String[3];
+		ClassExam1.main(s);
+		int res = MethodExam4.factorial(5);
+		System.out.println(res);
+		MethodExam3.printMultiTables(2, 9);
+
+	}
+
+}
