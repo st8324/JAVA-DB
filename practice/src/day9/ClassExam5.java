7bbab6a8c40f8f7cfebbbd2b9d1d905aab8d16c6
st8324 <stajun@naver.com>
2019-04-05 16:30:06 +0900

    9일차-Point 클래스 활용 예제

---
+++ practice/src/day9/ClassExam5.java
@@ -0,0 +1,19 @@
+package day9;
+
+public class ClassExam5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Point p = new Point(1,1);
+		p.print();
+		Point p1 = new Point();
+		p1.print();
+		Point p2 = new Point(p);
+		p2.print();
+		p2.move(3, 4);
+		p2.print();
+		System.out.println(p2.distance(0, 0));
+		
+	}
+
+}
