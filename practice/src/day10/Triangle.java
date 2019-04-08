82aef12e4f0a9667bbff99dd439469a5aa53daba
st8324 <stajun@naver.com>
2019-04-08 17:09:57 +0900

    10일차-Triagle 클래스 추가(Shape 클래스 상속)

---
+++ practice/src/day10/Triangle.java
@@ -0,0 +1,19 @@
+package day10;
+
+public class Triangle extends Shape {
+	double area;
+	double getArea(){
+		return area;
+	}
+	public Triangle(){}
+	public Triangle(int x, int y, int w, int h){
+		super(x,y,w,h);
+		area = w * h / 2.0;
+	}
+	@Override
+	public void print(){
+		System.out.println("삼각형");
+		super.print();
+		System.out.println("넓이 : " + area);
+	}
+}
