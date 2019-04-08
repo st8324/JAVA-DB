4127c3a2106772864cc59950eb1821bc71a546c0
st8324 <stajun@naver.com>
2019-04-08 14:30:35 +0900

    10일차-초기화 블록 예제

---
+++ practice/src/day10/Exam1.java
@@ -0,0 +1,25 @@
+package day10;
+
+public class Exam1 {
+
+	public static void main(String[] args) {
+		int []arr = new int[5];
+		arr[0] = 1;
+
+	}
+
+}
+class A{
+	public int num;
+	public static int cnt = 0;
+	{
+		num = 10;
+	}
+	static{
+		cnt = 10;
+	}
+}
+
+
+
+
