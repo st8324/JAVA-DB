b730301a375455eed0b0beed18d9d527a5495a46
st8324 <stajun@naver.com>
2019-04-08 16:20:08 +0900

    10일차-추상클래스 예제

---
+++ practice/src/day10/InheritanceExam3.java
@@ -0,0 +1,16 @@
+package day10;
+
+public class InheritanceExam3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+abstract class Shape2{
+	int x, y;
+	int w, h;
+	abstract void print();
+}
+
