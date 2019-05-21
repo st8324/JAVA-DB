c87cb55e1249c2d5e0997b36726df49863d04d79
st8324 <stajun@naver.com>
2019-05-21 16:47:35 +0900

    복습 - 5월 21일 - 1부터 10까지의 합 예제

---
+++ example/src/example1/exam1.java
@@ -0,0 +1,16 @@
+package example1;
+
+public class exam1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum;
+		int i;
+		for(sum=0, i=1; i<=10 ; i++ ){
+			sum += i;
+			//sum = sum + i;
+		}
+		System.out.println(sum);
+	}
+
+}
