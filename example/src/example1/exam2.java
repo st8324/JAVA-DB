44ffcba8da7306a78f8f1b996069ae39e35a5a35
st8324 <stajun@naver.com>
2019-05-21 16:47:46 +0900

    복습 - 5월 21일 - 배열 저장 예제

---
+++ example/src/example1/exam2.java
@@ -0,0 +1,17 @@
+package example1;
+
+public class exam2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] arr = new int[6];
+		for(int i = 0; i<arr.length; i++){
+			arr[i] = i+1;
+		}
+		for(int i = 0; i<arr.length; i++){
+			System.out.print(arr[i]+" ");
+		}
+		
+	}
+
+}
